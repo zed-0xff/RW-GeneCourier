c4f6e4fbe1566f6aa23c447a7eec9597b99f1d1d
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2023-04-20 13:33:29 +0300

    initilal

---
+++ Source/Patch_GeneUtility.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using Verse;
+using HarmonyLib;
+using RimWorld;
+
+namespace zed_0xff.GeneCourier {
+    [HarmonyPatch(typeof(GeneUtility), nameof(GeneUtility.ImplantXenogermItem))]
+    static class Patch_ImplantXenogermItem
+    {
+        static readonly GeneDef geneCourier = DefDatabase<GeneDef>.GetNamed("GeneCourier");
+
+        static void Postfix(Pawn pawn, Xenogerm xenogerm){
+            if( pawn == null || pawn.genes == null || xenogerm == null )
+                return;
+
+            List<GeneSet> genesets = GeneCache.Get(xenogerm);
+            if( genesets == null )
+                return;
+
+            foreach( Gene gene in pawn.genes.GenesListForReading ){
+                if( gene is Gene_Courier gc) {
+                    gc.AddGenesets(genesets);
+                    break;
+                }
+            }
+        }
+    }
+}
